357d74061625acc1ee905ee7dd711fc2f9bdd088
Rhitik_HBR <ritikbhandari17@gmail.com>
2025-03-19 23:57:17 +0545

    feat:initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-blue-700 text-white font-semibold h-40 flex flex-col  items-center justify-center text-2xl">
+        Ritik_hbr
+      </div>
+    </>
+  );
+}
+
+export default App;
